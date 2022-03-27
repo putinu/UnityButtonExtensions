dea997839fc32dcb4f4c4d63e5137f07dc55a5ff
putinu <putinu2521@gmail.com>
2022-03-27 13:38:33 +0900

    長押し機能の実装

--- Assets/ButtonExtensions/Scripts/ButtonExtension.cs
+++ Assets/ButtonExtensions/Scripts/ButtonExtension.cs
@@ -5,77 +5,90 @@ using UnityEngine.EventSystems;
 
 public class ButtonExtension : MonoBehaviour,
     IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler,
-    IPointerExitHandler, IPointerClickHandler
+    IPointerExitHandler
 {
     [SerializeField] private ButtonHandler buttonHandler;
     [SerializeField] private ButtonSetting buttonSetting;
-    [SerializeField] private bool enableDoubleClick = false;
 
     private bool _canDoubleClick = false;
     private bool _isClick = false;
-    private float _holdDownTime = 0.0f;
+    private bool _isPointerDown = false;
+    private bool _isPointerOn = false;
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        _isPointerDown = true;
         _isClick = true;
-        if (enableDoubleClick)
-        {
-            if (_canDoubleClick) return;
-            StartCoroutine(WaitDoubleClick());
-        }
-        else
-        {
-            // TODO: 長押し処理の実装を行う
-        }
+
+        StartCoroutine(WaitHoldDownOrUp());
+        if (_canDoubleClick) return;
+        StartCoroutine(WaitDoubleClick());
         
         _isClick = false;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        _isPointerDown = false;
         buttonHandler.OnPointerUp();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        _isPointerOn = true;
         buttonHandler.OnPointerEnter();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        _isPointerOn = false;
         buttonHandler.OnPointerExit();
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        buttonHandler.OnPointerClick();
-    }
-
+    /// <summary>
+    /// 指定時間内に２度目のクリック入力を待つ
+    /// </summary>
     private IEnumerator WaitDoubleClick()
     {
         _canDoubleClick = true;
-        var time = 0.0f;
-        while (time <= buttonSetting.maxDoubleClickInterval)
+        var elapsedTime = 0.0f;
+        while (elapsedTime <= buttonSetting.maxDoubleClickInterval)
         {
             yield return null;
             if (_isClick)
             {
-                DoubleClick();
+                buttonHandler.OnPointerDoubleClick();
                 break;
             }
-            time += Time.deltaTime;
+            elapsedTime += Time.deltaTime;
         }
 
         _canDoubleClick = false;
     }
 
-    private void DoubleClick()
+    /// <summary>
+    /// 長押しするか、ボタンが離されるまでまで待機する
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WaitHoldDownOrUp()
     {
-        buttonHandler.OnPointerDoubleClick();
-    }
+        var elapsedTime = 0.0f;
+        while (_isPointerDown)
+        {
+            yield return null;
+            if (elapsedTime >= buttonSetting.holdDownTime)
+            {
+                buttonHandler.OnPointerHoldDown();
+                yield break;
+            }
+            else if (!_isPointerOn)
+            {
+                yield break;
+            }
 
-    private void HoldDown()
-    {
-        buttonHandler.OnPointerHoldDown();
+            elapsedTime += Time.deltaTime;
+        }
+        // 以下離してしまった時の処理
+        buttonHandler.OnPointerClick();
     }
 }
